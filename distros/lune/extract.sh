ec2865218b5bac6a36eca95c7e10b9b77bd25ab2
Ondrej Jirman <megous@megous.com>
2020-09-08 16:48:15 +0200

    Update lune os

---
+++ distros/lune/extract.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+L=`losetup -P --show -f "$1"`
+
+mkdir -p m
+mount ${L}p2 m
+mount ${L}p1 m/boot
+bsdtar -cvf - -C m --numeric-owner . | zstd -z -4 - > rootfs.tar.zst
+umount m/boot
+umount m
+
+losetup -d $L
