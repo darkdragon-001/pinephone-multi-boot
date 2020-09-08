e6c0272b1a245b7c8faadd7d01d6e5d85dd8cbb0
Ondrej Jirman <megous@megous.com>
2020-09-08 14:45:10 +0200

    Add ubuntu touch

---
+++ distros/ut/extract.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+L=`losetup -P --show -f "$1"`
+
+mkdir -p m
+mount ${L}p9 m
+mount ${L}p4 m/boot
+bsdtar -cvf - -C m --numeric-owner . | zstd -z -4 - > rootfs.tar.zst
+umount m/boot
+umount m
+
+mount ${L}p6 m
+bsdtar -cvf - -C m --numeric-owner . | zstd -z -4 - > recovery.tar.zst
+umount m
+
+losetup -d $L
