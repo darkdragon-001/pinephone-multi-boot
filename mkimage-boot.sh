784e89286726d811b64027b18b18dddb10fef5f7
Ondrej Jirman <megous@megous.com>
2020-09-14 17:03:42 +0200

    Use real serial, so that we don't hog the udc (distros might want to use it)

--- mkimage-boot.sh
+++ mkimage-boot.sh
@@ -5,14 +5,13 @@ if [ "$(whoami)" != "root" ] ; then
 fi
 
 # loglevel=15
-serial="console=ttyGS0"
-#serial="console=ttyS0,115200 earlycon=ns16550a,mmio32,0x01c28000"
+serial="console=ttyS0,115200 earlycon=ns16550a,mmio32,0x01c28000"
 silent="quiet loglevel=0 systemd.show_status=false"
 bootargs_base="$serial $silent cma=256M console=tty1 consoleblank=0 panic=3 rw rootwait root=PARTUUID=12345678-02 rootfstype=btrfs rootflags=compress-force=zstd,nodatacow,subvol"
 kbuilds=../builds
 
 (
-	echo "device_id = Distro Demo Image 0.2beta"
+	echo "device_id = Distro Demo Image 2020-09-14"
 	no=0
 	for ddir in distros/*
 	do
