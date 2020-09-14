d0097b65e5837aa9108b9119ce266e737f08dcb5
Ondrej Jirman <megous@megous.com>
2020-09-14 04:56:55 +0200

    Add snapshots with original rootfs files for easy restore

--- mkimage-apply-fixes.sh
+++ mkimage-apply-fixes.sh
@@ -9,13 +9,14 @@ PASS='$6$nzZZGV65imLStmVz$u/Z1litGJh5tV2NmvzeirBiPkwWmhD0CQ.xRzdOV26vMxURbQUDW8N
 mkdir -p m/ut/android/{cache,data,factory,firmware,odm,persist,system{,/vendor}}
 touch m/ut/userdata/.writable_image
 
-for mdir in m/*
+for ddir in distros/*
 do
-	dist=${mdir#m/}
-	ddir="../../distros/$dist"
+	name=${ddir#distros/}
+	mdir=m/$name
 
 	(
 		cd $mdir || exit
+		ddir="../../$ddir"
 
 		sed -i '/mmcblk\|UUID/d' etc/fstab
 		sed -i "s#\$6\$.*#$PASS:::::::#" etc/shadow
@@ -31,8 +32,14 @@ EOF
 		do
 			dst="${src#$ddir/overrides/}"
 			chattr -i "$dst"
+			test -f ".xnux.eu/orig/$dst" || {
+				mkdir -p "$(dirname ".xnux.eu/orig/$dst")"
+				mv "$dst.bak" ".xnux.eu/orig/$dst"
+			}
 			cat "$src" > "$dst"
 			chattr +i "$dst"
 		done < <(find "$ddir/overrides" -type f -print0)		
 	)
+
+	test -f $mdir.orig || btrfs subvolume snapshot $mdir $mdir.orig
 done
