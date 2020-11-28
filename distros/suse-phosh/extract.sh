fd68eedc56cf95bd9204d81e5f683687728be5e8
Dark Dragon <darkdragon-001@web.de>
2020-11-28 23:02:03 +0100

    Add openSUSE.

---
+++ distros/suse-phosh/extract.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+L=`losetup -P --show -f "$1"`
+
+mkdir -p m
+mount ${L}p2 m
+bsdtar -cvf - -C m --numeric-owner . | zstd -z -T0 -19 - > rootfs.tar.zst
+umount m
+rmdir m
+
+losetup -d $L
